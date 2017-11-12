75133b8ce8da4f4c6d6c50f409e78e360e356305
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-11-12 18:59:04 +0100

    new

---
+++ src/main/java/com/redhat/chefapp/winepairing/WinePairingApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.chefapp.winepairing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinePairingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinePairingApplication.class, args);
+	}
+}
