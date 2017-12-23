3edf3139ffe99cce00a0bfbcec8ee5142d1e541f
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-12-23 10:18:11 +0100

    fixing controller

--- src/main/java/com/redhat/chefapp/winepairing/DBConfig.java
+++ src/main/java/com/redhat/chefapp/winepairing/DBConfig.java
@@ -6,25 +6,38 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Configuration
 public class DBConfig extends AbstractMongoConfiguration{
-    private static final String MONGO_DBNAME = "MONGO_DBNAME";
-    private static final String MONGO_HOSTNAME = "MONGO_HOSTNAME";
-    private static final String MONGO_PORT = "MONGO_PORT";
+    private static final String MONGODB_USER = "MONGODB_USER";
+    private static final String MONGODB_PASSWORD = "MONGODB_PASSWORD";
+    private static final String MONGODB_DATABASE = "MONGODB_DATABASE";
+    private static final String MONGODB_HOSTNAME = "MONGODB_HOSTNAME";
+    private static final String MONGODB_PORT = "MONGODB_PORT";
     
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        String hostname = System.getenv(MONGO_HOSTNAME) != null ? System.getenv(MONGO_HOSTNAME) : "localhost";
-        String port = System.getenv(MONGO_PORT) != null ? System.getenv(MONGO_PORT) : "27017";
+        String hostname = System.getenv(MONGODB_HOSTNAME) != null ? System.getenv(MONGODB_HOSTNAME) : "localhost";
+        String port = System.getenv(MONGODB_PORT) != null ? System.getenv(MONGODB_PORT) : "27017";
+        String user = System.getenv(MONGODB_USER);
+        String pass = System.getenv(MONGODB_PASSWORD);
+        
+        String uri = String.format("mongodb://%s/%s", hostname, port);
+        if (user != null && pass != null) {
+            uri = String.format("mongodb://%s:%s@%s/%s", user, pass, hostname, port);
+        }
 
-        return new MongoClient(hostname + ":" + port);
+        System.out.println("uri: " + uri);
+
+        return new MongoClient(new MongoClientURI(uri)
+);
     }
 
     @Override
     protected String getDatabaseName() {
-        return System.getenv(MONGO_DBNAME) != null ? System.getenv(MONGO_DBNAME) : "mydb";
+        return System.getenv(MONGODB_DATABASE) != null ? System.getenv(MONGODB_DATABASE) : "mydb";
     }
 }
 
