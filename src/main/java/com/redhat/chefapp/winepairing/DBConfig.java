1d19fa4c25b891bc180be0f58288405f8035acac
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-12-28 10:31:50 +0100

    env var changes

--- src/main/java/com/redhat/chefapp/winepairing/DBConfig.java
+++ src/main/java/com/redhat/chefapp/winepairing/DBConfig.java
@@ -13,14 +13,14 @@ public class DBConfig extends AbstractMongoConfiguration{
     private static final String MONGODB_USER = "MONGODB_USER";
     private static final String MONGODB_PASSWORD = "MONGODB_PASSWORD";
     private static final String MONGODB_DATABASE = "MONGODB_DATABASE";
-    private static final String MONGODB_HOSTNAME = "MONGODB_HOSTNAME";
-    private static final String MONGODB_PORT = "MONGODB_PORT";
+    private static final String DATABASE_SERVICE_NAME = "DATABASE_SERVICE_NAME";
+    private static final String DATABASE_SERVICE_PORT = "DATABASE_SERVICE_PORT";
     
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        String hostname = System.getenv(MONGODB_HOSTNAME) != null ? System.getenv(MONGODB_HOSTNAME) : "localhost";
-        String port = System.getenv(MONGODB_PORT) != null ? System.getenv(MONGODB_PORT) : "27017";
+        String hostname = System.getenv(DATABASE_SERVICE_NAME) != null ? System.getenv(DATABASE_SERVICE_NAME) : "localhost";
+        String port = System.getenv(DATABASE_SERVICE_PORT) != null ? System.getenv(DATABASE_SERVICE_PORT) : "27017";
         String user = System.getenv(MONGODB_USER);
         String pass = System.getenv(MONGODB_PASSWORD);
         String authSource = System.getenv(MONGODB_DATABASE);
